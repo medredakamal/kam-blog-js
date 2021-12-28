91f8fcccbe3d01215eb367d5bf60049535de8695
Med Reda Kamal <gencokm@gmail.com>
2021-12-29 00:46:19 +0100

    Get posts from API

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Hello JS
+// FUNC : Get posts from API (JSON Placeholder)
+async function loadPostsFromAPI() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const posts = await res.json()
+  return posts
+}
+
+// Init
+loadPostsFromAPI()
