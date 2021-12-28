25082ce18a5bbbeb5814423851c19ec11090271e
Med Reda Kamal <gencokm@gmail.com>
2021-12-29 00:49:30 +0100

    Add blog config + Limit posts from API
    Signed-off-by: Med Reda Kamal <gencokm@gmail.com>

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const blog_config = {
+  limit: 10,
+}
+
 // FUNC : Get posts from API (JSON Placeholder)
 async function loadPostsFromAPI() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit${blog_config.limit}`
+  )
   const posts = await res.json()
   return posts
 }
